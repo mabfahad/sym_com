0b77aa0d852fe0977a9872b58dc51cb774210450
Fahad <fahad.abdullah1280@gmail.com>
2021-08-02 01:24:09 +0600

    first output in symfony

---
+++ src/Controller/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController
+{
+    public function test()
+    {
+        return new Response('Learning Symfony is Fun! olo');
+    }
+}
